82980588d688c21b94acbeb98b7b39d58b548262
xiaonanln <xiaonanln@gmail.com>
2017-07-14 15:38:52 +0800

    add comments

--- goworld.go
+++ goworld.go
@@ -15,55 +15,76 @@ func Run(delegate server.IServerDelegate) {
 	server.Run(delegate)
 }
 
+// Register the entity type
+//
+// returns the entity type description object which can be used to define more properties
+// of entity type
 func RegisterEntity(typeName string, entityPtr entity.IEntity) *entity.EntityTypeDesc {
 	return entity.RegisterEntity(typeName, entityPtr)
 }
 
-//func createEntity(typeName string) {
-//	server.createEntity(typeName)
-//}
-
+// Create a space with specified kind in any game server
 func CreateSpaceAnywhere(kind int) {
 	entity.CreateSpaceAnywhere(kind)
 }
 
-func CreateSpaceLocally(kind int) {
-	entity.CreateSpaceLocally(kind)
+// Create a space with specified kind in the local game server
+//
+// returns the space EntityID
+func CreateSpaceLocally(kind int) EntityID {
+	return entity.CreateSpaceLocally(kind)
 }
 
+// Create a entity on the local server
+//
+// returns EntityID
 func CreateEntityLocally(typeName string) EntityID {
 	return entity.CreateEntityLocally(typeName, nil, nil)
 }
 
+// Create a entity on any server
 func CreateEntityAnywhere(typeName string) {
 	entity.CreateEntityAnywhere(typeName)
 }
 
+// Load the specified entity from entity storage
 func LoadEntityAnywhere(typeName string, entityID EntityID) {
 	entity.LoadEntityAnywhere(typeName, entityID)
 }
 
+// Get the set of EntityIDs that provides the specified service
 func GetServiceProviders(serviceName string) entity.EntityIDSet {
 	return entity.GetServiceProviders(serviceName)
 }
 
 // Get all saved entity ids in storage, may take long time and block the main routine
+//
+// returns result in callback
 func ListEntityIDs(typeName string, callback storage.ListCallbackFunc) {
 	storage.ListEntityIDs(typeName, callback)
 }
 
+// Check if entityID exists in entity storage
+//
+// returns result in callback
 func Exists(typeName string, entityID EntityID, callback storage.ExistsCallbackFunc) {
 	storage.Exists(typeName, entityID, callback)
 }
 
+// Get entity by EntityID
 func GetEntity(id EntityID) *entity.Entity {
 	return entity.GetEntity(id)
 }
 
+// Get the local server ID
+//
+// server ID is a uint16 number starts from 1, which should be different for each servers
+// server ID is also in the game config section name of goworld.ini
 func GetServerID() uint16 {
 	return server.GetServerID()
 }
 
+// Creates a new MapAttr
 func MapAttr() *entity.MapAttr {
 	return entity.NewMapAttr()
 }
