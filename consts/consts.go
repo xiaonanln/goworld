b0e52d5c537b8c9f2885304c1d2e9f5cccb387b3
xiaonanln <xiaonanln@gmail.com>
2017-07-20 23:58:22 +0800

    tune network params

--- consts/consts.go
+++ consts/consts.go
@@ -15,7 +15,7 @@ const (
 	// For Server & Gate
 	GAME_SERVICE_PACKET_QUEUE_SIZE = 10000 // packet queue size
 	// For Server
-	SERVER_TICK_INTERVAL = time.Millisecond * 10 // server tick interval => affects timer resolution
+	SERVER_TICK_INTERVAL = time.Millisecond * 10 // server tick interval => affect timer resolution
 
 	DISPATCHER_CLIENT_WRITE_BUFFER_SIZE = 1024 * 1024
 	DISPATCHER_CLIENT_READ_BUFFER_SIZE  = 1024 * 1024
