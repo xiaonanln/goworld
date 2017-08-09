f19a7c7eb068bc622622ac1d01a8f08eeb2a9bdd
xiaonanln <xiaonanln@gmail.com>
2017-08-09 13:13:12 +0800

    fixing lint errors

--- engine/common/collections.go
+++ engine/common/collections.go
@@ -1,20 +1,25 @@
 package common
 
+// StringSet is a set of strings
 type StringSet map[string]struct{}
 
+// Contains checks if Stringset contains the string
 func (ss StringSet) Contains(elem string) bool {
 	_, ok := ss[elem]
 	return ok
 }
 
+// Add adds the string to StringSet
 func (ss StringSet) Add(elem string) {
 	ss[elem] = struct{}{}
 }
 
+// Remove removes the string from StringList
 func (ss StringSet) Remove(elem string) {
 	delete(ss, elem)
 }
 
+// ToList convert StringSet to string slice
 func (ss StringSet) ToList() []string {
 	keys := make([]string, 0, len(ss))
 	for s := range ss {
@@ -23,8 +28,10 @@ func (ss StringSet) ToList() []string {
 	return keys
 }
 
+// StringList is a list of string (slice)
 type StringList []string
 
+// Remove removes the string from StringList
 func (sl *StringList) Remove(elem string) {
 	widx := 0
 	cpsl := *sl
@@ -42,10 +49,12 @@ func (sl *StringList) Remove(elem string) {
 	*sl = cpsl[:widx]
 }
 
+// Append add the string to the end of StringList
 func (sl *StringList) Append(elem string) {
 	*sl = append(*sl, elem)
 }
 
+// Find get the index of string in StringList, returns -1 if not found
 func (sl *StringList) Find(s string) int {
 	for idx, elem := range *sl {
 		if elem == s {
