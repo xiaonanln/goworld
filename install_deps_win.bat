a658399eaa3885bff134465597089fda9c0537e9
xiaonanln <xiaonanln@gmail.com>
2017-08-10 21:37:20 +0800

    install_deps_win.bat

---
+++ install_deps_win.bat
@@ -0,0 +1,18 @@
+pip install psutil
+go get golang.org/x/net/context
+go get golang.org/x/net/websocket
+go get github.com/xiaonanln/go-xnsyncutil/xnsyncutil
+go get github.com/xiaonanln/goTimer
+go get github.com/xiaonanln/typeconv
+go get github.com/Sirupsen/logrus
+go get github.com/garyburd/redigo/redis
+go get github.com/google/btree
+go get github.com/pkg/errors
+go get github.com/bmizerany/assert
+go get gopkg.in/eapache/queue.v1
+go get gopkg.in/ini.v1
+go get gopkg.in/mgo.v2
+go get gopkg.in/vmihailenco/msgpack.v2
+go get gopkg.in/natefinch/lumberjack.v2
+@echo All dependencies are installed successfully!
+@pause
