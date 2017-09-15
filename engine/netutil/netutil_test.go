79990b45d81463ed3930f6d79401cbd9e3ff7666
LinNan <hzlinnan@debian-owncloud-opt>
2017-09-15 19:59:30 +0800

    fix test error

--- engine/netutil/netutil_test.go
+++ engine/netutil/netutil_test.go
@@ -92,39 +92,37 @@ func TestCompressedConnection(t *testing.T) {
 }
 
 func TestPacketConnection(t *testing.T) {
-	for compressed := 0; compressed <= 1; compressed += 1 {
 
-		_conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", PORT))
-		if err != nil {
-			t.Errorf("connect error: %s", err)
+	_conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", PORT))
+	if err != nil {
+		t.Errorf("connect error: %s", err)
+	}
+
+	conn := NewPacketConnection(NetConnection{_conn})
+
+	for i := 0; i < 10; i++ {
+		var PAYLOAD_LEN uint32 = uint32(rand.Intn(4096 + 1))
+		gwlog.Infof("Testing with payload %v", PAYLOAD_LEN)
+
+		packet := conn.NewPacket()
+		for j := uint32(0); j < PAYLOAD_LEN; j++ {
+			packet.AppendByte(byte(rand.Intn(256)))
 		}
-
-		conn := NewPacketConnection(NetConnection{_conn}, compressed != 0)
-
-		for i := 0; i < 10; i++ {
-			var PAYLOAD_LEN uint32 = uint32(rand.Intn(4096 + 1))
-			gwlog.Infof("Testing with payload %v", PAYLOAD_LEN)
-
-			packet := conn.NewPacket()
-			for j := uint32(0); j < PAYLOAD_LEN; j++ {
-				packet.AppendByte(byte(rand.Intn(256)))
-			}
-			if packet.GetPayloadLen() != PAYLOAD_LEN {
-				t.Errorf("payload should be %d, but is %d", PAYLOAD_LEN, packet.GetPayloadLen())
-			}
-			conn.SendPacket(packet)
-			conn.Flush("Test")
-			recvPacket, err := conn.RecvPacket()
-			if err != nil {
-				t.Error(err)
-			}
-			if packet.GetPayloadLen() != recvPacket.GetPayloadLen() {
-				t.Errorf("send packet len %d, but recv len %d", packet.GetPayloadLen(), recvPacket.GetPayloadLen())
-			}
-			for i := uint32(0); i < packet.GetPayloadLen(); i++ {
-				if packet.Payload()[i] != recvPacket.Payload()[i] {
-					t.Errorf("send packet and recv packet mismatch on byte index %d", i)
-				}
+		if packet.GetPayloadLen() != PAYLOAD_LEN {
+			t.Errorf("payload should be %d, but is %d", PAYLOAD_LEN, packet.GetPayloadLen())
+		}
+		conn.SendPacket(packet)
+		conn.Flush("Test")
+		recvPacket, err := conn.RecvPacket()
+		if err != nil {
+			t.Error(err)
+		}
+		if packet.GetPayloadLen() != recvPacket.GetPayloadLen() {
+			t.Errorf("send packet len %d, but recv len %d", packet.GetPayloadLen(), recvPacket.GetPayloadLen())
+		}
+		for i := uint32(0); i < packet.GetPayloadLen(); i++ {
+			if packet.Payload()[i] != recvPacket.Payload()[i] {
+				t.Errorf("send packet and recv packet mismatch on byte index %d", i)
 			}
 		}
 	}
