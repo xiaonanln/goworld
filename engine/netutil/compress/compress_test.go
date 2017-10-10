2e075b76a6fad40a5ded2ed37080f29f83fc70ac
xiaonanln <xiaonanln@gmail.com>
2017-10-10 10:47:52 +0800

    bench -> snappy best

--- engine/netutil/compress/compress_test.go
+++ engine/netutil/compress/compress_test.go
@@ -5,6 +5,49 @@ import (
 	"testing"
 )
 
+func BenchmarkSnappyCompressor(b *testing.B) {
+	benchmarkCompressor(b, NewSnappyCompressor())
+}
+
+func BenchmarkLz4Compressor(b *testing.B) {
+	benchmarkCompressor(b, NewLz4Compressor())
+}
+
+func BenchmarkLzwCompressor(b *testing.B) {
+	benchmarkCompressor(b, NewLzwCompressor())
+}
+
+func BenchmarkFlateCompressor(b *testing.B) {
+	benchmarkCompressor(b, NewFlateCompressor())
+}
+
+func BenchmarkZLibCompressor(b *testing.B) {
+	benchmarkCompressor(b, NewZlibCompressor())
+}
+
+func benchmarkCompressor(b_ *testing.B, cr Compressor) {
+	dataSize := 100 * 1024
+	b := make([]byte, dataSize)
+	for j := 0; j < dataSize; j++ {
+		b[j] = byte(97 + rand.Intn(3))
+	}
+
+	for n := 0; n < b_.N; n++ {
+		var c []byte
+		var err error
+		if c, err = cr.Compress(b, c); err != nil {
+			panic(err)
+		}
+
+		rb := make([]byte, len(b))
+		if err = cr.Decompress(c, rb); err != nil {
+			panic(err)
+		}
+
+		dataSize = dataSize * 2
+	}
+}
+
 func TestSnappyCompressor(t *testing.T) {
 	testCompressor(t, NewSnappyCompressor())
 }
@@ -17,9 +60,9 @@ func TestFlateCompressor(t *testing.T) {
 	testCompressor(t, NewFlateCompressor())
 }
 
-//func TestZlibCompressor(t *testing.T) {
-//	testCompressor(t, NewZlibCompressor())
-//}
+func TestZlibCompressor(t *testing.T) {
+	testCompressor(t, NewZlibCompressor())
+}
 
 func TestLzwCompressor(t *testing.T) {
 	testCompressor(t, NewLzwCompressor())
