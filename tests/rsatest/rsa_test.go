3c23af0a5ad020a3dce4b77138fcea3e39a1e8bc
xiaonanln <xiaonanln@gmail.com>
2017-10-10 14:35:13 +0800

    fix rsa tsest

--- tests/rsatest/rsa_test.go
+++ tests/rsatest/rsa_test.go
@@ -8,24 +8,39 @@ import (
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestRSA(t *testing.T) {
-	pemFile, err := os.Open("h39.pem")
-	checkError(err)
-	defer pemFile.Close()
-	pemData, err := ioutil.ReadAll(pemFile)
-	checkError(err)
+	pemData := []byte(`
+-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQDZsfv1qscqYdy4vY+P4e3cAtmvppXQcRvrF1cB4drkv0haU24Y
+7m5qYtT52Kr539RdbKKdLAM6s20lWy7+5C0DgacdwYWd/7PeCELyEipZJL07Vro7
+Ate8Bfjya+wltGK9+XNUIHiumUKULW4KDx21+1NLAUeJ6PeW+DAkmJWF6QIDAQAB
+AoGBAJlNxenTQj6OfCl9FMR2jlMJjtMrtQT9InQEE7m3m7bLHeC+MCJOhmNVBjaM
+ZpthDORdxIZ6oCuOf6Z2+Dl35lntGFh5J7S34UP2BWzF1IyyQfySCNexGNHKT1G1
+XKQtHmtc2gWWthEg+S6ciIyw2IGrrP2Rke81vYHExPrexf0hAkEA9Izb0MiYsMCB
+/jemLJB0Lb3Y/B8xjGjQFFBQT7bmwBVjvZWZVpnMnXi9sWGdgUpxsCuAIROXjZ40
+IRZ2C9EouwJBAOPjPvV8Sgw4vaseOqlJvSq/C/pIFx6RVznDGlc8bRg7SgTPpjHG
+4G+M3mVgpCX1a/EU1mB+fhiJ2LAZ/pTtY6sCQGaW9NwIWu3DRIVGCSMm0mYh/3X9
+DAcwLSJoctiODQ1Fq9rreDE5QfpJnaJdJfsIJNtX1F+L3YceeBXtW0Ynz2MCQBI8
+9KP274Is5FkWkUFNKnuKUK4WKOuEXEO+LpR+vIhs7k6WQ8nGDd4/mujoJBr5mkrw
+DPwqA3N5TMNDQVGv8gMCQQCaKGJgWYgvo3/milFfImbp+m7/Y3vCptarldXrYQWO
+AQjxwc71ZGBFDITYvdgJM1MTqc8xQek1FXn1vfpy2c6O
+-----END RSA PRIVATE KEY-----
+`)
 	privateBlock, _ := pem.Decode(pemData)
 	priKey, err := x509.ParsePKCS1PrivateKey(privateBlock.Bytes)
 	checkError(err)
 
-	pubFile, err := os.Open("h39.pub")
-	checkError(err)
-	defer pubFile.Close()
-	pubData, err := ioutil.ReadAll(pubFile)
+	pubData := []byte(`
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZsfv1qscqYdy4vY+P4e3cAtmv
+ppXQcRvrF1cB4drkv0haU24Y7m5qYtT52Kr539RdbKKdLAM6s20lWy7+5C0Dgacd
+wYWd/7PeCELyEipZJL07Vro7Ate8Bfjya+wltGK9+XNUIHiumUKULW4KDx21+1NL
+AUeJ6PeW+DAkmJWF6QIDAQAB
+-----END PUBLIC KEY-----
+`)
 	checkError(err)
 	pubBlock, _ := pem.Decode(pubData)
 	pubKeyValue, err := x509.ParsePKIXPublicKey(pubBlock.Bytes)
@@ -33,7 +48,7 @@ func TestRSA(t *testing.T) {
 	pubKey := pubKeyValue.(*rsa.PublicKey)
 
 	var plainText []byte
-	for i := 0; i < 256-50; i++ {
+	for i := 0; i < 50; i++ {
 		plainText = append(plainText, 'A')
 	}
 
