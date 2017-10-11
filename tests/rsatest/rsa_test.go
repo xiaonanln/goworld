4f659c893932d768ba1b9db000d7994ce88d118b
xiaonanln <xiaonanln@gmail.com>
2017-10-11 10:49:55 +0800

    add rsa private & public keys

--- tests/rsatest/rsa_test.go
+++ tests/rsatest/rsa_test.go
@@ -7,41 +7,21 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
 func TestRSA(t *testing.T) {
-	pemData := []byte(`
------BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQDZsfv1qscqYdy4vY+P4e3cAtmvppXQcRvrF1cB4drkv0haU24Y
-7m5qYtT52Kr539RdbKKdLAM6s20lWy7+5C0DgacdwYWd/7PeCELyEipZJL07Vro7
-Ate8Bfjya+wltGK9+XNUIHiumUKULW4KDx21+1NLAUeJ6PeW+DAkmJWF6QIDAQAB
-AoGBAJlNxenTQj6OfCl9FMR2jlMJjtMrtQT9InQEE7m3m7bLHeC+MCJOhmNVBjaM
-ZpthDORdxIZ6oCuOf6Z2+Dl35lntGFh5J7S34UP2BWzF1IyyQfySCNexGNHKT1G1
-XKQtHmtc2gWWthEg+S6ciIyw2IGrrP2Rke81vYHExPrexf0hAkEA9Izb0MiYsMCB
-/jemLJB0Lb3Y/B8xjGjQFFBQT7bmwBVjvZWZVpnMnXi9sWGdgUpxsCuAIROXjZ40
-IRZ2C9EouwJBAOPjPvV8Sgw4vaseOqlJvSq/C/pIFx6RVznDGlc8bRg7SgTPpjHG
-4G+M3mVgpCX1a/EU1mB+fhiJ2LAZ/pTtY6sCQGaW9NwIWu3DRIVGCSMm0mYh/3X9
-DAcwLSJoctiODQ1Fq9rreDE5QfpJnaJdJfsIJNtX1F+L3YceeBXtW0Ynz2MCQBI8
-9KP274Is5FkWkUFNKnuKUK4WKOuEXEO+LpR+vIhs7k6WQ8nGDd4/mujoJBr5mkrw
-DPwqA3N5TMNDQVGv8gMCQQCaKGJgWYgvo3/milFfImbp+m7/Y3vCptarldXrYQWO
-AQjxwc71ZGBFDITYvdgJM1MTqc8xQek1FXn1vfpy2c6O
------END RSA PRIVATE KEY-----
-`)
+	pemData, err := ioutil.ReadFile("../../goworld.pem")
+	checkError(err)
 	privateBlock, _ := pem.Decode(pemData)
 	priKey, err := x509.ParsePKCS1PrivateKey(privateBlock.Bytes)
 	checkError(err)
 
-	pubData := []byte(`
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZsfv1qscqYdy4vY+P4e3cAtmv
-ppXQcRvrF1cB4drkv0haU24Y7m5qYtT52Kr539RdbKKdLAM6s20lWy7+5C0Dgacd
-wYWd/7PeCELyEipZJL07Vro7Ate8Bfjya+wltGK9+XNUIHiumUKULW4KDx21+1NL
-AUeJ6PeW+DAkmJWF6QIDAQAB
------END PUBLIC KEY-----
-`)
+	pubData, err := ioutil.ReadFile("../../goworld.pub")
 	checkError(err)
 	pubBlock, _ := pem.Decode(pubData)
+
 	pubKeyValue, err := x509.ParsePKIXPublicKey(pubBlock.Bytes)
 	checkError(err)
 	pubKey := pubKeyValue.(*rsa.PublicKey)
@@ -51,17 +31,23 @@ AUeJ6PeW+DAkmJWF6QIDAQAB
 		plainText = append(plainText, 'A')
 	}
 
-	encryptText, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, pubKey, plainText, nil)
-	checkError(err)
-	fmt.Printf("Encrypt text: %s", encryptText)
+	k := (pubKey.N.BitLen() + 7) / 8
+	maxPlainSize := k - 2*sha1.New().Size() - 2
+	fmt.Printf("Public key K = %d, max plain text size = %d\n", k, maxPlainSize)
 
-	decryptText, err := rsa.DecryptOAEP(sha1.New(), rand.Reader, priKey, encryptText, nil)
+	hash := sha1.New()
+	encryptText, err := rsa.EncryptOAEP(hash, rand.Reader, pubKey, plainText, nil)
+	checkError(err)
+	fmt.Printf("Encrypt text: (%d)%s\n", len(encryptText), encryptText)
+
+	decryptText, err := rsa.DecryptOAEP(hash, rand.Reader, priKey, encryptText, nil)
 	checkError(err)
 
-	fmt.Printf("Decrypt: %s", string(decryptText))
+	fmt.Printf("Decrypt: %s\n", string(decryptText))
 	if string(decryptText) != string(plainText) {
 		t.Fatalf("decrypt text is wrong")
 	}
+
 }
 
 func checkError(e error) {
