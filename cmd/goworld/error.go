60f2680392de911d341ac08ecab178d58a244ebc
xiaonanln <xiaonanln@gmail.com>
2017-10-31 00:25:51 +0800

    goworld build

--- cmd/goworld/error.go
+++ cmd/goworld/error.go
@@ -6,17 +6,17 @@ import (
 )
 
 func showMsgAndQuit(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format+"\n", a...)
+	fmt.Fprintf(os.Stderr, "! "+format+"\n", a...)
 	os.Exit(2)
 }
 
 func showMsg(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format+"\n", a...)
+	fmt.Fprintf(os.Stderr, "> "+format+"\n", a...)
 }
 
 func checkErrorOrQuit(err error, msg string) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", msg, err)
+		fmt.Fprintf(os.Stderr, "! %s: %v\n", msg, err)
 		os.Exit(2)
 	}
 }
