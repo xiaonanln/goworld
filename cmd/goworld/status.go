33a4b33e5c00221831ded1f696be9775a386f9c5
Seis <xiaonanln@gmail.com>
2017-10-31 15:24:48 +0800

    remove psutil in gopkg

--- cmd/goworld/status.go
+++ cmd/goworld/status.go
@@ -1,14 +1,12 @@
 package main
 
-import "github.com/shirou/gopsutil/process"
-
 func detectRunningServer() {
-	pids, err := process.Pids()
-	checkErrorOrQuit(err, "get pids failed")
-	for _, pid := range pids {
-		proc, err := process.NewProcess(pid)
-		checkErrorOrQuit(err, "new process failed")
-		exe, _ := proc.Exe()
-		println("process", exe)
-	}
+	//pids, err := process.Pids()
+	//checkErrorOrQuit(err, "get pids failed")
+	//for _, pid := range pids {
+	//	proc, err := process.NewProcess(pid)
+	//	checkErrorOrQuit(err, "new process failed")
+	//	exe, _ := proc.Exe()
+	//	println("process", exe)
+	//}
 }
