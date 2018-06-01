74496799b2d0a92ab5de26c27af330cc688d89b9
xiaonanln <xiaonanln@gmail.com>
2018-06-01 22:51:28 +0800

    service discovery using etcd

--- engine/srvdis/service_discovery.go
+++ engine/srvdis/service_discovery.go
@@ -5,6 +5,8 @@ import (
 
 	"context"
 
+	"sync"
+
 	"github.com/coreos/etcd/clientv3"
 	"github.com/xiaonanln/goworld/engine/gwlog"
 	"github.com/xiaonanln/goworld/engine/gwutils"
@@ -22,27 +24,38 @@ type ServiceDelegate interface {
 }
 
 func Startup(ctx context.Context, etcdEndPoints []string, namespace string, delegate ServiceDelegate) {
-	srvdisNamespace = namespace
+	go func() {
+		srvdisNamespace = namespace
 
-	cfg := clientv3.Config{
-		Endpoints:   etcdEndPoints,
-		DialTimeout: time.Second,
-		//Transport: client.DefaultTransport,
-		// set timeout per request to fail fast when the target endpoint is unavailable
-		//HeaderTimeoutPerRequest: time.Second,
-		Context: ctx,
-	}
+		cfg := clientv3.Config{
+			Endpoints:   etcdEndPoints,
+			DialTimeout: time.Second,
+			//Transport: client.DefaultTransport,
+			// set timeout per request to fail fast when the target endpoint is unavailable
+			//HeaderTimeoutPerRequest: time.Second,
+			Context: ctx,
+		}
 
-	cli, err := clientv3.New(cfg)
-	if err != nil {
-		gwlog.Panic(err)
-	}
+		cli, err := clientv3.New(cfg)
+		if err != nil {
+			gwlog.Panic(err)
+		}
 
-	go gwutils.RepeatUntilPanicless(func() {
-		registerRoutine(ctx, cli, delegate)
-	})
+		defer cli.Close()
 
-	go gwutils.RepeatUntilPanicless(func() {
-		watchRoutine(ctx, cli, delegate)
-	})
+		var wait sync.WaitGroup
+		wait.Add(2)
+
+		go gwutils.RepeatUntilPanicless(func() {
+			registerRoutine(ctx, cli, delegate)
+			wait.Done()
+		})
+
+		go gwutils.RepeatUntilPanicless(func() {
+			//watchRoutine(ctx, cli, delegate)
+			wait.Done()
+		})
+
+		wait.Wait()
+	}()
 }
