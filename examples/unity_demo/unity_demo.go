a9064dd78a4568670afbdf7b3724c8b5ad41abc5
xiaonanln <xiaonanln@gmail.com>
2018-06-15 23:22:03 +0800

    fix unity demo

--- examples/unity_demo/unity_demo.go
+++ examples/unity_demo/unity_demo.go
@@ -14,8 +14,8 @@ var (
 func main() {
 	goworld.RegisterSpace(&MySpace{}) // 注册自定义的Space类型
 
-	goworld.RegisterEntity("OnlineService", &OnlineService{})
-	goworld.RegisterEntity("SpaceService", &SpaceService{})
+	goworld.RegisterService("OnlineService", &OnlineService{})
+	goworld.RegisterService("SpaceService", &SpaceService{})
 	// 注册Account类型
 	goworld.RegisterEntity("Account", &Account{})
 	// 注册Monster类型
