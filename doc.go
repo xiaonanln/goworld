579ae1b8303b59bf5f3c1b1f7ed55159ae7b2748
Seis <xiaonanln@gmail.com>
2018-08-11 09:37:02 +0800

    doc

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+
+GoWorld
+
+GoWorld is a distributed game server engine. GoWorld adopts a Space-Entity framework for game server programming.
+Entities can migrate between spaces by calling `EnterSpace`. Entities can call each other using EntityID which is a
+global unique identifier for each entity.
+
+Multiprocessing
+
+
+
+*/
+package goworld
