d4b80ce59c045bd20bc1e9091634ef66cac767d4
eidng8 <cheung.jackey@gmail.com>
2019-12-16 12:57:10 +0800

    decouples the dispatcher from `goworld` package

--- cmd/goworld/build.go
+++ cmd/goworld/build.go
@@ -26,7 +26,14 @@ func buildServer(sid ServerID) {
 	buildDirectory(serverPath)
 }
 
-func buildComponent(component string) {
+// buildComponent starts the actual build process of the specified component.
+// In order to support customization and use `goworld` as a pure dependency.
+// We first check if there's a corresponding directory in the work space
+// for the component.
+// If there's no such directory in work space, we assume that it's
+// using the `goworld` way, where components are inside `goworld`'s
+// sub-directory.
+func buildComponent(sid ServerID, component string) {
 	showMsg(fmt.Sprintf("go build %s ...", component))
 	buildDirectory(env.GetComponentDir(component))
 }
@@ -34,6 +41,7 @@ func buildComponent(component string) {
 func buildDirectory(dir string) {
 	var err error
 
+	fmt.Printf("building directory %s ...", dir)
 	err = os.Chdir(dir)
 	checkErrorOrQuit(err, "Failed to change directory.")
 
@@ -42,29 +50,20 @@ func buildDirectory(dir string) {
 		checkErrorOrQuit(err, "Couldn't change to workspace directory")
 	}()
 
-	cmd := exec.Command("go", "build", ".")
+	cmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		filepath.Join(binPath()),
+		".",
+	)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	err = cmd.Run()
 	checkErrorOrQuit(err, fmt.Sprintf("Failed to build %s", dir))
 
-	moveBinary(dir)
+	// moveBinary(dir)
 
 	return
-}
-
-func moveBinary(dir string) {
-	file := filepath.Base(dir) + BinaryExtension
-	fullPath := filepath.Join(dir, file)
-	dest := filepath.Join(env.GetBinaryDir(), file)
-	if !isfile(fullPath) {
-		showMsgAndQuit("Couldn't find the binary: %s", fullPath)
-	}
-
-	err := os.Rename(fullPath, dest)
-	checkErrorOrQuit(err, "Failed to move binary to workspace")
-
-	err = chmod(dest, 0544)
-	checkErrorOrQuit(err, "Failed to set binary permission")
 }
